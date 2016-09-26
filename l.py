42e316e5d9482a27940bf6aa0a64b98ee85ee864
Pandaklez <belkannkl@gmail.com>
2016-09-26 16:25:05 +0300

    Update l.py

--- l.py
+++ l.py
@@ -3,3 +3,6 @@
 print("Content-type: text/html")
 print()
 print("<h1>Я локальный сервер!</h1>")
+
+#ЭТОТ ФАЙЛ ДОЛЖЕН ЛЕЖАТЬ В ПАПКЕ cgi-bin
+#ЕГО РЕЗУЛЬТАТ ПЕЧАТАЕТСЯ НА http://localhost:8000/cgi-bin/l.py
