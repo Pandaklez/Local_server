3d2c29075537692cb7787b374d665690bc2edd69
Pandaklez <belkannkl@gmail.com>
2016-09-26 16:22:58 +0300

    Update hhh.py

--- hhh.py
+++ hhh.py
@@ -2,3 +2,5 @@ from http.server import HTTPServer, CGIHTTPRequestHandler
 server_address = ("", 8000)
 httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
 httpd.serve_forever()
+
+# ЭТОТ ФАЙЛ СОЗДАЁТ http://localhost:8000
